15e9396d7b69f988f90f75ef5e1d40eb2fda8b56
David <david@wearethorn.org>
2020-02-18 12:14:24 +0000

    update configs

---
+++ config.js
@@ -0,0 +1,4 @@
+const appConfig={
+    'API_KEY': 'AIzaSyDmRXLjNKM31VJOmdFYOa4slHKbA1WagwI',
+    'API_DEV_KEY': 'SECRET'
+};
--- config_example.js
+++
@@ -1,3 +0,0 @@
-const appConfig={
-    'API_KEY': 'TODO: add your key and rename to config.js'
-};
